cb371c423b38628f0537cad01a5fdc65028fb330
shubhamgoelgithub <goel.shubham291997@gmail.com>
2025-03-06 16:23:38 +0530

    fix update user and format image

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/userModels.js";
 import Job from "../models/jobModels.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
   // alternative way
@@ -26,8 +26,9 @@ export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
+    // await fs.unlink(req.file.path); it is removed coz we are not retreiving the image inside the public/uploads folder
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -3,18 +3,16 @@
  *   All rights reserved.
  */
 import multer from "multer";
+import DataURIParser from "datauri/parser";
+import path from "path";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // set the directory where uploaded files will be stored
-    cb(null, "public/uploads");
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname;
-    // set the name of the uploaded file
-    cb(null, fileName);
-  },
-});
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+const parser = new DataURIParser();
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalName).toString();
+  return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
